6ef46743c9e46d92134930454efb064f73c6b424
clement bernard <c.bernard817@laposte.net>
2023-10-11 21:28:32 +0200

    bugfix CreationVote

--- src/models/voteModel.js
+++ src/models/voteModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let voteSchema = new Schema ({
-    vote: {
+    note: {
         type: Number,
         require: true
     },
